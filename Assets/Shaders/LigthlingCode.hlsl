09bd72ae5fa340aa949d18764d29d426043c359a
freecardinal <112091651+lolarenzo2000@users.noreply.github.com>
2026-04-10 07:42:58 -0500

    ligth

---
+++ Assets/Shaders/LigthlingCode.hlsl
@@ -0,0 +1,18 @@
+//#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+void MainLight_float(float3 PositionWS, out float3 Direction, out float3 Color)
+{
+    #if defined(SHADERGRAPH_PREVIEW)
+    Direction = normalize(float3(1,1,-1));
+    Color = 1.0f;
+    #else 
+    float4 shadowCoord = TransformWorldToShadowCoord(PositionWS);
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    #endif
+
+
+    
+    
+}
