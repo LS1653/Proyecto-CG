9c5719245c81859282385e1b03e70d559071d73d
LS1653 <estebanpuerta2006@gmail.com>
2026-04-10 07:41:18 -0500

    Clase-16-hlsl

---
+++ Assets/Esteban/particulasepm/Shader/Include/CsutomLigthing.hlsl
@@ -0,0 +1,17 @@
+//#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+
+
+void MainLight_float(float3 PositionWS, out float3 Direction, out float3 Color)//float -> 32 bits
+{
+    #if defined(SHADERGRAPH_PREVIEW)
+    
+    Direction = normalize(float3(1, 1, -1)); //Intrincecamente normalizado, pero se normaliza de nuevo para asegurarse de que es un vector unitario 
+    Color = 1.0f;
+    #else
+    float4 shadowCoord = TransformWorldToShadowCoord(PositionWS);
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    #endif
+}
