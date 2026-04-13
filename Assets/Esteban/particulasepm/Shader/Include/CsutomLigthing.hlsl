55ff61837a90071b6588a7255ab8138d3bcd6bfb
LS1653 <estebanpuerta2006@gmail.com>
2026-04-13 07:45:33 -0500

    clase-17-luces-sombras-interactuables-con-shaders

--- Assets/Esteban/particulasepm/Shader/Include/CsutomLigthing.hlsl
+++ Assets/Esteban/particulasepm/Shader/Include/CsutomLigthing.hlsl
@@ -2,16 +2,56 @@
 
 
 
-void MainLight_float(float3 PositionWS, out float3 Direction, out float3 Color)//float -> 32 bits
+void MainLight_float(float3 PositionWS, out float3 Direction, out float3 Color, out float ShadowAttenuation)//float -> 32 bits
 {
     #if defined(SHADERGRAPH_PREVIEW)
     
     Direction = normalize(float3(1, 1, -1)); //Intrincecamente normalizado, pero se normaliza de nuevo para asegurarse de que es un vector unitario 
     Color = 1.0f;
+    ShadowAttenuation = 1.0f; // No shadow attenuation in preview mode
     #else
     float4 shadowCoord = TransformWorldToShadowCoord(PositionWS);
     Light mainLight = GetMainLight(shadowCoord);
     Direction = mainLight.direction;
     Color = mainLight.color;
+    ShadowAttenuation = mainLight.shadowAttenuation;
     #endif
+}
+
+
+void AdditionalLightsSimple_float(float3 PositionWS, float3 ViewDirectionWS, float3 NormalWS, out float3 Lit)
+{
+#ifdef SHADERGRAPH_PREVIEW
+    Lit = 0;
+#else
+    uint additionalLightCount = GetAdditionalLightsCount();
+    
+    //TODO: Forward+
+
+    LIGHT_LOOP_BEGIN(additionalLightCount)
+
+    Light currentLight = GetAdditionalLight(lightIndex, PositionWS);
+
+    //Diffuse
+    float lambert = dot(currentLight.direction, NormalWS);
+    lambert = max(0, lambert * 0.5f + 0.5f); //half lambert
+    float3 diffuse = lambert
+    * currentLight.color
+    * currentLight.shadowAttenuation
+    * currentLight.distanceAttenuation;
+
+    //Specular
+    float3 h = normalize(ViewDirectionWS + currentLight.direction);
+    float blinnPhong = dot(h, NormalWS);
+    blinnPhong = max(0, blinnPhong);
+    blinnPhong = pow(blinnPhong, 60.0f);
+    float3 specular = blinnPhong
+    * currentLight.color
+    * currentLight.shadowAttenuation
+    * currentLight.distanceAttenuation;
+
+    Lit += diffuse + specular;
+    LIGHT_LOOP_END
+    
+#endif
 }
