be99619b86509d509aaf106f7aded552fa9bdd08
B09S112est <B09S112est@MEDAULAS.UPB>
2026-04-13 07:44:39 -0500

    A lot of lights stuff. Like, a lot. Damn.

--- Assets/Particles/Shaders/Include/CustomLighting.hlsl
+++ Assets/Particles/Shaders/Include/CustomLighting.hlsl
@@ -2,11 +2,12 @@
 
 //Preprocessor Directive ---> "#if" o "#ifdef"
 
-void MainLight_float(float3 PositionWS, out float3 Direction, out float3 Color) //"_float" --> Sufijo de precisión
+void MainLight_float(float3 PositionWS, out float3 Direction, out float3 Color, out float ShadowAttenuation) //"_float" --> Sufijo de precisión
 {
-    #if defined(SHADERGRAPH_PREVIEW)
+    #ifdef SHADERGRAPH_PREVIEW
     Direction = normalize(float3(1, 1, -1));
     Color = 1.0f;
+    ShadowAttenuation = 1.0f;
 
     #else
     float4 shadowCoord = TransformWorldToShadowCoord(PositionWS);
@@ -14,5 +15,36 @@ void MainLight_float(float3 PositionWS, out float3 Direction, out float3 Color) 
 
     Direction = mainLight.direction;
     Color = mainLight.color;
+    ShadowAttenuation = mainLight.shadowAttenuation;
+    #endif
+}
+
+void AdditionalLightsSimple_float(float3 PositionWS, float3 ViewDirectionWS, float3 NormalWS, out float3 Lit)
+{
+    #ifdef SHADERGRAPH_PREVIEW
+    Lit = 0;
+    #else
+    uint additionalLightCount = GetAdditionalLightsCount();
+
+    //TODO: Forward+
+
+    LIGHT_LOOP_BEGIN(additionalLightCount)
+    Light currentLight = GetAdditionalLight(lightIndex, PositionWS);
+    
+    //Diffuse
+    float lambert = dot(currentLight.direction, NormalWS);
+    lambert = max(0, lambert * 0.5f + 0.5f); //Half Lambert
+    float3 diffuse = lambert * currentLight.color * currentLight.shadowAttenuation * currentLight.distanceAttenuation;
+
+    //Specular
+    float3 h = normalize(ViewDirectionWS + currentLight.direction);
+    float blinnPhong = dot(h, NormalWS);
+    blinnPhong = max(0, blinnPhog);
+    blinnPhong = pow(blinnPhong, 60.0f); //Blinn Phong
+    float3 specular = blinnPhong * currentLight.color * currentLight.shadowAttenuation * currentLight.distanceAttenuation;
+    
+    Lit += diffuse + specular;
+    LIGHT_LOOP_END
+
     #endif
 }
