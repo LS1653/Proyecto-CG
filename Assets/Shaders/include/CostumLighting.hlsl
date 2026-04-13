7c1a6059921f815d71cfb28add3f9deca694ef7a
No. <157375381+catflyx@users.noreply.github.com>
2026-04-13 07:46:21 -0500

    Clase 17 - Efecto 4
    Abril 13 2026
    
    Iluminación básica
    
    Sombra y más de que la luz principal. El código algo está mal idk,

--- Assets/Shaders/include/CostumLighting.hlsl
+++ Assets/Shaders/include/CostumLighting.hlsl
@@ -1,10 +1,11 @@
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+// #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
-void MainLight_float(float3 PositionWS, out float3 Direction, out float3 Color) // float -> 32 bits
+void MainLight_float(float3 PositionWS, out float3 Direction, out float3 Color, out float ShadowAttenuation) // float -> 32 bits
 {
     #ifdef SHADERGRAPH_PREVIEW
     Direction = normalize(float3(1, 1, -1));
     Color = 1.0f;
+    ShadowAttenuation = 1.0f;
     
     #else
     float4 shadowCoord = TransformWorldToShadowCoord(PositionWS);
@@ -12,6 +13,41 @@ void MainLight_float(float3 PositionWS, out float3 Direction, out float3 Color) 
     Light mainLight = GetMainLight(shadowCoord);
     Direction = mainLight.direction;
     Color = mainLight.color;
+    ShadowAttenuation = mainLight.shadowAttenuation;
     
+    #endif
+}
+
+void AdditionalLightsSimple_float(float3 PositionWS, float3 ViewDirectionWS, float3 NormalWS, out float3 Lit)
+{
+    #ifdef SHADERGRAPH_PREVIEW
+    Lit = 0;
+
+    #else
+    unit additionalLightCount = GetAdditionalLightsCount();
+    
+    //TODO: Forward+
+    
+    LIGHT_LOOP_BEGIN(additionalLightCount)
+
+    Light currentLight = GetAdditionalLight(lightIndex, PositionWS);
+    
+    //Diffuse
+    float lambert = dot(currentLight.direction, NormalWS);
+    lambert = max(0, lambert * 0.5f + 0.5f); //half lambert
+    
+    float diffuse = lambert * currentLight.color * currentLight.shadowAttenuation * currentLight.distanceAttenuation;
+    
+    //Specular
+    float3 h = normalize(ViewDirectionWS + currentLight.direction);
+    float blinnPhong = dot(h, NormalWS);
+    blinnPhong = max(0, blinnPhong);
+    blinnPhong = pow(blinnPhong, 60.0f);
+    float3 specular = blinnPhong * currentLight.color * currentLight.shadowAttenuation * currentLight.distanceAttenuation;
+    
+    Lit += diffuse + specular;
+    
+    LIGHT_LOOP_END
+
     #endif
 }
