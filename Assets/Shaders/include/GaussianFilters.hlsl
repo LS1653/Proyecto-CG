8ec69ffe5f98a1493ff2f05d3640c364e9735a6a
No. <157375381+catflyx@users.noreply.github.com>
2026-05-08 07:09:13 -0500

    Clase 21 - Efecto 5
    Mayo 8 2026
    
    Código, y subgraph.

--- Assets/Shaders/include/GaussianFilters.hlsl
+++ Assets/Shaders/include/GaussianFilters.hlsl
@@ -3,7 +3,14 @@
 
 float SampleGaussianKernel3x3(float2 coords)
 {
-    return 1;
+    float index = (coords.x + 1) + ((-coords.y) + 1) * 3;
+    float GaussianKernel3x3[9] =
+    {
+        0.06, 0.125, 0.06,
+        0.125, 0.25, 0.125,
+        0.06, 0.125, 0.06
+    };
+    return GaussianKernel3x3[clamp(index, 0, 8)];
 }
 
 void GaussianFilter3x3_float(float2 UV, float2 ScreenSize, float FilterDistance, out float4 FilteredImage)
@@ -14,7 +21,7 @@ void GaussianFilter3x3_float(float2 UV, float2 ScreenSize, float FilterDistance,
         for (int x = -1; x <= 1; x++)
         {
             //Load Texture 2D LOD Utiliza las coordenadas de pixel (0 - ancho / 0-alto) y no hace operaciones de LOD
-            float2 pixelCoords = UV * ScreenSize + float2(x, y);
+            float2 pixelCoords = UV * ScreenSize + float2(x, y) * FilterDistance;
             FilteredImage += LOAD_TEXTURE2D_X_LOD(_BlitTexture, pixelCoords, 0) * SampleGaussianKernel3x3(float2(x, y));
         }
     }
